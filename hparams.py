103d0f7773597d2d3421aea94930a48cfc1f0e78
bogihsu <b03901071@ntu.edu.tw>
2019-06-04 06:53:27 +0000

    dataset

--- hparams.py
+++ hparams.py
@@ -5,7 +5,6 @@ class hparams:
 	################################
 	# Data Parameters              #
 	################################
-	max_input = 140 # drop long text input
 	text_cleaners=['english_cleaners']
 
 	################################
@@ -26,32 +25,32 @@ class hparams:
 	# Model Parameters             #
 	################################
 	n_symbols = len(symbols)
-	symbols_embedding_dim = 256 # 512
+	symbols_embedding_dim = 512
 
 	# Encoder parameters
 	encoder_kernel_size = 5
 	encoder_n_convolutions = 3
-	encoder_embedding_dim = 256 # 512
+	encoder_embedding_dim = 512
 
 	# Decoder parameters
 	n_frames_per_step = 1
-	decoder_rnn_dim = 512 # 1024
-	prenet_dim = 128 # 256
+	decoder_rnn_dim = 1024
+	prenet_dim = 256
 	max_decoder_steps = 1000
 	gate_threshold = 0.5
 	p_attention_dropout = 0.1
 	p_decoder_dropout = 0.1
 
 	# Attention parameters
-	attention_rnn_dim = 512 # 1024
-	attention_dim = 256 # 128
+	attention_rnn_dim = 1024
+	attention_dim = 128
 
 	# Location Layer parameters
 	attention_location_n_filters = 32
 	attention_location_kernel_size = 31
 
 	# Mel-post processing network parameters
-	postnet_embedding_dim = 256 # 512
+	postnet_embedding_dim = 512
 	postnet_kernel_size = 5
 	postnet_n_convolutions = 5
 
